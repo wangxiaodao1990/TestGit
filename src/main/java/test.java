9f62b42cce7ce3df19613103120560f02c6ba96c
wangxiaodao1992 <798472490@qq.com>
2017-12-03 23:06:41 +0800

    beizhi

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -1,7 +1,7 @@
 public class test {
     public static void main(String[] args) {
         int a=10;
-        int b=1000312321321;
+        int b=100031232132134234324qweweqwewq;
         int sum =a+b;
         System.out.printf("chengxiaodao"+sum);
     }
