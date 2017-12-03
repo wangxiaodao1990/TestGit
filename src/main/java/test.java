a4e3a24781f6cc97e1bc4bd153f0c97461d1ca7c
wangxiaodao1992 <798472490@qq.com>
2017-12-03 22:59:41 +0800

    231321

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -1,7 +1,7 @@
 public class test {
     public static void main(String[] args) {
         int a=10;
-        int b=10;
+        int b=1000;
         int sum =a+b;
         System.out.printf("chengxiaodao"+sum);
     }
